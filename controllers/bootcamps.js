7ad98e40e09236dd1576aec40704dd84d8c3ae6a
Nikhil Pothuru <nikhilpothuru@Nikhils-MacBook-Pro-3.local>
2020-03-04 21:57:59 -0800

    Added model and created  create controller

---
+++ controllers/bootcamps.js
@@ -0,0 +1,41 @@
+const Bootcamp = require('../models/Bootcamp');
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: 'true', msg: 'GET' });
+};
+
+// @desc    Get a single bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: 'true', msg: 'GET w/ ID' });
+};
+
+// @desc    Create new bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
+  //res.status(200).json({ success: 'true', msg: 'POST' });
+};
+
+// @desc    Update bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ success: 'true', msg: 'PUT' });
+};
+
+// @desc    Delete bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ success: 'true', msg: 'DELETE' });
+};
--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// Dev Logger Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(
-    `Server running in env ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server running in env ${process.env.NODE_ENV} mode on port ${PORT}`.blue
+      .bold.underline
   )
 );
+
+// Handle unhandled rejections
+process.on('unhandled rejection', (err, promise) => {
+  console.log(`Error: ${err.message}.red.underline.bold`);
+  // Close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
